466e2d8d8d8b9d6b097da403d25f4beb35f2d8a5
ststeiger <quandary82@hailmail.net>
2015-02-17 09:27:03 +0100

    Rev4

---
+++ SQLMerge/FileHelper.cs
@@ -0,0 +1,91 @@
+﻿
+namespace SQLMerge
+{
+
+
+    internal class FileHelper
+    {
+
+
+        internal static System.IO.FileInfo[] GetSqlFilesSorted(string strPath)
+        {
+            //System.IO.Directory.GetFiles(strPath, "*.sql", System.IO.SearchOption.TopDirectoryOnly);
+            System.IO.FileInfo[] afiSQLfiles = GetSqlFiles(strPath);
+
+            // Array.Sort(afiSQLfiles);
+            System.Array.Sort(afiSQLfiles, StringHelper.SortFileNames);
+
+            // System.Array.Sort(afiSQLfiles, delegate(System.IO.FileInfo f1, System.IO.FileInfo f2)
+            // {
+            //     return System.StringComparer.InvariantCultureIgnoreCase.Compare(f1.Name, f2.Name);
+            // });
+
+
+
+            // In C# 3 this becomes slightly simpler:
+            // Array.Sort(afiSQLfiles, (f1, f2) => f1.Name.CompareTo(f2.Name));
+            // Or you can use a StringComparer if you want to use a case-insensitive sort order:
+            // Array.Sort(afiSQLfiles, (x, y) => StringComparer.OrdinalIgnoreCase.Compare(x.Name, y.Name));
+
+
+            //System.IO.FileInfo[] afiForDebug = new System.IO.FileInfo[] { 
+            //    new System.IO.FileInfo(
+            //        System.IO.Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "sqltestfile.txt")
+            //    ) 
+            //};
+
+            //return afiForDebug;
+
+            return afiSQLfiles;
+        } // End Function GetSqlFilesSorted
+
+
+        // SQLMerge.FileHelper.GetSqlFiles(@"");
+        internal static System.IO.FileInfo[] GetSqlFiles(string searchFolder)
+        {
+            return GetSqlFiles(searchFolder, System.IO.SearchOption.TopDirectoryOnly);
+        } // End Function GetRasterImages
+
+
+        // SQLMerge.FileHelper.GetSqlFiles();
+        internal static System.IO.FileInfo[] GetSqlFiles(string searchFolder, System.IO.SearchOption searchOption)
+        {
+            string[] filters = new string[] { "sql", "psql"};
+            return GetFilesFrom(searchFolder, filters, searchOption);
+        } // End Function GetRasterImages
+
+
+        // Linux case-insensitive multi-extension search
+        internal static System.IO.FileInfo[] GetFilesFrom(string searchFolder, string[] filters, System.IO.SearchOption searchOption)
+        {
+            System.IO.FileInfo[] arrRes = null;
+            System.Collections.Generic.List<System.IO.FileInfo> filesFound = new System.Collections.Generic.List<System.IO.FileInfo>();
+
+            System.IO.DirectoryInfo diDirInfo = new System.IO.DirectoryInfo(searchFolder);
+
+            System.IO.FileInfo[] filez = diDirInfo.GetFiles("*.*", searchOption);
+
+            for (int i = 0; i < filters.Length; ++i)
+            {
+                string extension = "." + filters[i];
+
+                for (int j = 0; j < filez.Length; ++j)
+                {
+                    if (filez[j].Name.EndsWith(extension, System.StringComparison.InvariantCultureIgnoreCase))
+                        filesFound.Add(filez[j]);
+                } // Next j 
+
+            } // Next i
+
+            arrRes = filesFound.ToArray();
+            filesFound.Clear();
+            filesFound = null;
+
+            return arrRes;
+        } // End Function GetFilesFrom
+
+
+    }
+
+
+}
---
+++ SQLMerge/Program.cs
@@ -0,0 +1,156 @@
+﻿
+namespace SQLMerge
+{
+
+
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            // Test.ReplaceTestFileContent();
+
+            string strPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string strFileName = System.IO.Path.Combine(strPath, "AllInOne.sql");
+
+            System.IO.FileInfo[] afiSQLfiles = FileHelper.GetSqlFilesSorted(strPath);
+            System.Console.WriteLine("Total number of sql files to be merged: {0}", afiSQLfiles.Length);
+
+            System.Collections.Generic.List<string> lsFileList = new System.Collections.Generic.List<string>();
+
+
+            string strFileContent = string.Empty;
+            foreach (System.IO.FileInfo fiThisFile in afiSQLfiles)
+            {
+                if (!System.StringComparer.InvariantCultureIgnoreCase.Equals(fiThisFile.Name, strFileName))
+                {
+                    System.Console.WriteLine("Adding content of file " + fiThisFile.Name);
+                    strFileContent = GetFileContent(fiThisFile);
+
+                    lsFileList.Add(strFileContent);
+                } // End if (!StringComparer.OrdinalIgnoreCase.Equals(fiThisFile.Name, strFileName))
+                else
+                    System.Console.WriteLine("Preventing the probably wrong addition of content from file \"" + strFileName + "\"");
+            } // Next fiThisFile
+
+            if (System.IO.File.Exists(strFileName))
+                System.IO.File.Delete(strFileName);
+
+            WriteToFile(lsFileList, strFileName);
+
+            System.Console.WriteLine(System.Environment.NewLine);
+            System.Console.WriteLine(" --- Press any key to continue --- ");
+            System.Console.ReadKey();
+        } // End Sub Main
+
+
+        public static string GetFileContent(System.IO.FileInfo fiThisFile)
+        {
+            System.Text.StringBuilder sb = null;
+            string strFileContent = string.Empty;
+            string strBatchSeparator = "GO";
+
+            try
+            {
+                // Specify file, instructions, and privelegdes
+                using (System.IO.FileStream fs = new System.IO.FileStream(fiThisFile.FullName, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                {
+
+                    // Create a new stream to read from a file
+                    using (System.IO.StreamReader sr = new System.IO.StreamReader(fs, System.Text.Encoding.Default))
+                    {
+                        string strFileName = fiThisFile.Name.Replace("'", "''");
+                        sb = new System.Text.StringBuilder();
+
+                        sb.Append(StringHelper.RepeatNewLine(1));
+                        sb.Append("PRINT 'Begin Executing \"");
+                        sb.Append(strFileName);
+                        sb.Append("\"' ");
+                        sb.Append(StringHelper.RepeatNewLine(2));
+                        sb.Append(strBatchSeparator);
+                        sb.Append(" ");
+                        sb.Append(StringHelper.RepeatNewLine(3));
+
+                        //strFileContent = strFileContent + sr.ReadToEnd(); // Read contents of file into a string
+                        sb.Append(sr.ReadToEnd());
+
+                        // Remove the newlines when created using sp_RPT_DEBUG_PrintVarcharMax 
+                        sb = sb.Replace("#@NEW@#_@#LINE@#\r\n", "");
+
+                        sb.Append(StringHelper.RepeatNewLine(2));
+                        sb.Append(strBatchSeparator);
+                        sb.Append(" ");
+                        sb.Append(StringHelper.RepeatNewLine(3));
+                        sb.Append("PRINT 'Done Executing \"");
+                        sb.Append(strFileName);
+                        sb.Append("\"' ");
+                        sb.Append(StringHelper.RepeatNewLine(3));
+                        sb.Append(strBatchSeparator);
+                        sb.Append(" ");
+                        sb.Append(StringHelper.RepeatNewLine(4));
+
+                        strFileContent = sb.ToString();
+                        sr.Close(); // Close StreamReader
+                    } // End Using sr
+
+                    fs.Close(); // Close file
+                } // End Using fs
+
+            } // End try
+            catch (System.Exception ex)
+            {
+                System.Console.Write("Reading file: \"" + fiThisFile.Name + "\" has failed because: " + ex.Message);
+                //MessageBox.Show("Reading of file: " + strFilename + " has failed because: " + ex.ToString());
+            } // End catch
+
+            sb.Length = 0;
+            sb = null;
+            return strFileContent;
+        } // End Function GetFileContent
+
+
+        public static void WriteToFile(System.Collections.Generic.List<string> lsInputFileList, string strFileName)
+        {
+            if (lsInputFileList == null || lsInputFileList.Count == 0)
+            {
+                System.Console.WriteLine("No files to be merged. Doing nothing.");
+                return;
+            }
+
+            try
+            {
+                // Specify file, instructions, and privelegdes
+                using (System.IO.FileStream fsOutputFile = new System.IO.FileStream(strFileName, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write))
+                {
+
+                    // Create a new stream to write to the file
+                    using (System.IO.StreamWriter sw = new System.IO.StreamWriter(fsOutputFile, System.Text.Encoding.UTF8))
+                    {
+
+                        foreach (string strThisFile in lsInputFileList)
+                        {
+                            // Write a string to the file
+                            sw.WriteLine(strThisFile);
+                        } // Next strThisFile
+
+                        sw.Close(); // Close StreamWriter
+                    } // End Using sw
+
+                    fsOutputFile.Close(); // Close file
+                } // End Using sw
+
+            } // End try
+            catch (System.Exception ex)
+            {
+                System.Console.Write("Writing to file: " + strFileName + " has failed because: " + ex.Message);
+                //MessageBox.Show("Writing to file: " + strFileName + " has failed because: " + ex.ToString());
+            } // End catch
+
+        } // End Sub WriteToFile
+
+
+    } // End Class Program
+
+
+} // End Namespace SQLMerge
