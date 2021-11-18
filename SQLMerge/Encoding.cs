b6758a4908b233cca2ba9e74be3bd0748008b4f2
ststeiger <quandary82@hailmail.net>
2021-11-18 10:23:06 +0100

    Bugfix - never trust code copied from stackoverflow...

--- SQLMerge/Encoding.cs
+++ SQLMerge/Encoding.cs
@@ -46,51 +46,81 @@ namespace SQLMerge
                 file.Close();
             } // End Using file 
 
-            if (b.Length >= 4 && b[0] == 0x00 && b[1] == 0x00 && b[2] == 0xFE && b[3] == 0xFF)
+            if (b.Length >= 4 && b[0] == 0x00 && b[1] == 0x00 && b[2] == 0xFE && b[3] == 0xFF) // UTF32-BE 
                 return System.Text.Encoding.GetEncoding("utf-32BE"); // UTF-32, big-endian 
-            else if (b.Length >= 4 && b[0] == 0xFF && b[1] == 0xFE && b[2] == 0x00 && b[3] == 0x00)
+            else if (b.Length >= 4 && b[0] == 0xFF && b[1] == 0xFE && b[2] == 0x00 && b[3] == 0x00) // UTF32-LE
                 return System.Text.Encoding.UTF32; // UTF-32, little-endian
-            else if (b.Length >= 2 && b[0] == 0xFE && b[1] == 0xFF)
-                return System.Text.Encoding.BigEndianUnicode; // UTF-16, big-endian
-            else if (b.Length >= 2 && b[0] == 0xFF && b[1] == 0xFE)
-                return System.Text.Encoding.Unicode; // UTF-16, little-endian
-            else if (b.Length >= 3 && b[0] == 0xEF && b[1] == 0xBB && b[2] == 0xBF)
-                return System.Text.Encoding.UTF8;  // UTF-8
-            else if (b.Length >= 3 && b[0] == 0x2b && b[1] == 0x2f && b[2] == 0x76)
+                                                   // https://en.wikipedia.org/wiki/Byte_order_mark#cite_note-14    
+            else if (b.Length >= 4 && b[0] == 0x2b && b[1] == 0x2f && b[2] == 0x76 && (b[3] == 0x38 || b[3] == 0x39 || b[3] == 0x2B || b[3] == 0x2F)) // UTF7
                 return System.Text.Encoding.UTF7;  // UTF-7
-
+            else if (b.Length >= 3 && b[0] == 0xEF && b[1] == 0xBB && b[2] == 0xBF) // UTF-8
+                return System.Text.Encoding.UTF8;  // UTF-8
+            else if (b.Length >= 2 && b[0] == 0xFE && b[1] == 0xFF) // UTF16-BE
+                return System.Text.Encoding.BigEndianUnicode; // UTF-16, big-endian
+            else if (b.Length >= 2 && b[0] == 0xFF && b[1] == 0xFE) // UTF16-LE
+                return System.Text.Encoding.Unicode; // UTF-16, little-endian
 
             // Maybe there is a future encoding ...
             // PS: The above yields more than this - this doesn't find UTF7 ...
             if (thorough)
             {
+                System.Collections.Generic.List<System.Collections.Generic.KeyValuePair<System.Text.Encoding, byte[]>> lsPreambles =
+                    new System.Collections.Generic.List<System.Collections.Generic.KeyValuePair<System.Text.Encoding, byte[]>>();
+
                 foreach (System.Text.EncodingInfo ei in System.Text.Encoding.GetEncodings())
                 {
                     System.Text.Encoding enc = ei.GetEncoding();
 
                     byte[] preamble = enc.GetPreamble();
+
+                    if (preamble == null)
+                        continue;
+
                     if (preamble.Length == 0)
                         continue;
 
                     if (preamble.Length > b.Length)
                         continue;
 
-                    for (int i = 0; i < preamble.Length; ++i)
+                    System.Collections.Generic.KeyValuePair<System.Text.Encoding, byte[]> kvp =
+                        new System.Collections.Generic.KeyValuePair<System.Text.Encoding, byte[]>(enc, preamble);
+
+                    lsPreambles.Add(kvp);
+                } // Next ei
+
+                // li.Sort((a, b) => a.CompareTo(b)); // ascending sort
+                // li.Sort((a, b) => b.CompareTo(a)); // descending sort
+                lsPreambles.Sort(
+                    delegate (
+                        System.Collections.Generic.KeyValuePair<System.Text.Encoding, byte[]> kvp1,
+                        System.Collections.Generic.KeyValuePair<System.Text.Encoding, byte[]> kvp2)
                     {
-                        if (b[i] != preamble[i])
+                        return kvp2.Value.Length.CompareTo(kvp1.Value.Length);
+                    }
+                );
+
+
+                for (int j = 0; j < lsPreambles.Count; ++j)
+                {
+                    for (int i = 0; i < lsPreambles[j].Value.Length; ++i)
+                    {
+                        if (b[i] != lsPreambles[j].Value[i])
                         {
-                            goto NextEncoding;
+                            goto NEXT_J_AND_NOT_NEXT_I;
                         }
                     } // Next i 
 
-                    return enc;
-                NextEncoding:
-                    continue;
-                } // Next ei
+                    return lsPreambles[j].Key;
+                NEXT_J_AND_NOT_NEXT_I: continue;
+                } // Next j 
+
             } // End if (thorough)
 
             return null;
         } // End Function BomInfo 
+
+
+
 
 
         public static System.Text.Encoding DetectOrGuessEncoding(string fileName)
@@ -107,7 +137,7 @@ namespace SQLMerge
 
             System.ConsoleColor origBack = System.ConsoleColor.Black;
             System.ConsoleColor origFore = System.ConsoleColor.White;
-            
+
 
             if (withOutput)
             {
